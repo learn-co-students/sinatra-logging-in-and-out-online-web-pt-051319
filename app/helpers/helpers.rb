afb3aa0d19a571cc9d4100a9fecaee96ebea0f25
jamievullo <jvullo@protonmail.com>
2019-08-22 14:03:55 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,11 @@
 class Helpers
 
     def self.current_user(session)
-
-
+        @user = User.find_by_id(session[:user_id])
     end
 
-    def self.logged_in?(session)
-
-
+    def self.is_logged_in?(session)
+        !!session[:user_id]
     end
 
 end
