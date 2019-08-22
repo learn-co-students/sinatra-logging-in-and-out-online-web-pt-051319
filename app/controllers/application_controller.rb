143874b2ee6a4ac9c2f3e8f2807a2f7773c2e496
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-22 23:11:02 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,26 +7,30 @@ class ApplicationController < Sinatra::Base
   end
 
 
-    get '/' do
-      erb :index
-    end
+  get '/' do
+  erb :index
+  end
 
-    post '/login' do
-      user = User.find_by(username: params["username"], password: params["password"])
-      if user
-        session[:user_id] = user.id
-        redirect to '/account'
-      else
-        erb :error
-      end
-    end
+  post '/login' do
+   @user = User.find_by(:username => params[:username])
+   if @user != nil && @user.password == params[:password]
+     session[:user_id] = @user.id
+     redirect to '/account'
+   end
+   erb :error
+  end
 
-    get '/account' do
-      erb :account
-    end
+  get '/account' do
+   @current_user = User.find_by_id(session[:user_id])
+   if @current_user
+     erb :account
+   else
+     erb :error
+   end
+  end
 
-    get '/logout' do
-      session.clear
-      redirect to '/'
-    end
+  get '/logout' do
+   session.clear
+   redirect to '/'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 20190822230804) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "password"
     t.decimal "balance"
   end
