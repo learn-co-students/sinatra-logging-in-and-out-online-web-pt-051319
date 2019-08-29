4fb52532aa25ce77f658db0a5d434cc6bd2adf2c
KristinHannah <kristinhannahk@gmail.com>
2019-08-29 13:42:55 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
 class Helpers
+
+    def self.current_user(session) 
+        @user = User.find_by(id: session[:user_id])
+        @user 
+    end 
+
+    def self.is_logged_in?(session)
+        if session[:user_id]
+            TRUE 
+        else     
+            FALSE 
+        end 
+    end 
+    
 end
+
